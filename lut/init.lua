98f103e05cdc2a66db309315dcf9fde9412249ab
LunaCI <lunaci@luadist.org>
2018-06-08 02:04:51 +0000

    Update to version 1.1.0-1

--- lut/init.lua
+++ lut/init.lua
@@ -6,8 +6,7 @@
   
   <html><a href="https://github.com/lubyk/lut"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a></html>
 
-  This module is part of the [lubyk](http://lubyk.org) project. *MIT license*
-  &copy; Gaspard Bucher 2014.
+  *MIT license* &copy Gaspard Bucher 2014.
 
   ## Installation
   
@@ -15,23 +14,43 @@
 
     $ luarocks install lut
   
-  With [luadist](http://luadist.org):
-
-    $ luadist install lut
-  
 --]]--------------------
 local lub = require 'lub'
 local lib = lub.Autoload 'lut'
 
 -- Current version of 'lut' respecting [semantic versioning](http://semver.org).
-lib.VERSION = '1.0.3'
+lib.VERSION = '1.1.0'
 
 -- Library dependencies
 lib.DEPENDS = { -- doc
   -- Compatible with Lua 5.1, 5.2 and LuaJIT
   'lua >= 5.1, < 5.3',
   -- Uses [Lubyk base library](http://doc.lubyk.org/lub.html)
-  'lub >= 1.0.3, < 1.1',
+  'lub >= 1.0.3, < 2',
+}
+
+-- nodoc
+lib.DESCRIPTION = {
+  summary = "Utility module for lubyk. Contains a documentation generator and testing facilities.",
+  detailed = [[
+    lub.Doc: a powerful documentation generator for Lua code with support for
+    litterate programming, cross-reference linking, images, latex math, etc.
+    
+    lub.Test: unit testing framework.
+
+    lub.Builder: helper to generate luarocks and CMake files.
+
+    Read the documentation at http://doc.lubyk.org/lut.html. 
+  ]],
+  homepage = "http://doc.lubyk.org/lut.html",
+  author   = "Gaspard Bucher",
+  license  = "MIT",
+}
+
+-- nodoc
+lib.BUILD = {
+  github   = 'lubyk',
+  pure_lua = true,
 }
 
 return lib
